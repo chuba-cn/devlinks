5f8afa1ab24aa4993399ce4b9f5e810eaa566148
Chuba . C <chinemelumchubanwene57@gmail.com>
2024-07-23 23:15:49 +0100

    add backend api routes & functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={instrumentSans.className}>{children}</body>
+      <body className={instrumentSans.className}>
+        {children}
+      </body>
     </html>
   );
 }
